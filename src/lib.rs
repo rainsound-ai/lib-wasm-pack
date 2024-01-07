0d73331c6777a6499f1efd7422f6e9df6bc01cb1
Luca Bailey <mail@photon.garden>
2024-01-07 12:40:24 -0500

    Use a uuid when saving the temporary file to avoid collisions if multiple build processes are running at once.

--- src/lib.rs
+++ src/lib.rs
@@ -99,7 +99,8 @@ fn get_cli_executable_file() -> Result<PathBuf, WasmPackError> {
         cli_executable_bytes.len()
     );
 
-    let temp_file_name = format!("wasm-pack-{}-v{}", platform, CRATE_VERSION);
+    let uuid = uuid::Uuid::new_v4().to_string();
+    let temp_file_name = format!("wasm-pack-{}-v{}-{}", platform, CRATE_VERSION, uuid);
     let temp_file_path = std::env::current_dir()
         .map_err(WasmPackError::CouldntSaveCliExecutableToTemporaryFile)?
         .join("target")
